3cfafcf7aad49066a5bb11b58f9e4b93b2f4898f
indicium15 <67154087+indicium15@users.noreply.github.com>
2023-08-30 15:05:37 +0800

    Generic main file

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <tuple>
+#include <unordered_map>
+#include <cstring>
+#include <typeinfo>
+
+using namespace std;
+
+int main(){
+  cout << "Hello World";
+  return 0;
+}
