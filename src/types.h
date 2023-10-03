fd0ba2b6136064edcb2e0aff9b7f1e4cba916ea2
AryanSethi20 <aryansethi20@gmail.com>
2023-10-03 16:49:52 +0800

    New structures to follow for block address and offset, and records

---
+++ src/types.h
@@ -0,0 +1,27 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+#include <cstdlib>
+
+// Defines an address of a record stored as a block address with an offset.
+struct Address
+{
+  void *blockAddress;
+  uint8_t offset;
+};
+
+// Defines a single movie record (read from data file).
+struct Record{
+    float fg_pct_home;
+    float ft_pct_home;
+    float fg3_pct_home;
+    int game_date;
+    unsigned short int recordID;
+    uint8_t team_id_home;
+    uint8_t pts_home;
+    uint8_t ast_home;
+    uint8_t reb_home;
+    bool home_team_wins;
+};
+
+#endif
